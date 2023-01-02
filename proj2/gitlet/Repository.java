93a9a72bdbd32b6228ae9552da198b992f78f4c7
LongPeng <longpeng96@qq.com>
2023-01-02 09:18:10 +0800

    Repository v2

--- proj2/gitlet/Repository.java
+++ proj2/gitlet/Repository.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 import static gitlet.Utils.*;
@@ -38,7 +37,8 @@ public class Repository {
     public static final File GITLET_DIR = join(CWD, ".gitlet");
 
     /**
-     * The .gitlet/stageForAdd directory, used to store the files readied for commit
+     * The .gitlet/stageForAdd directory,
+     * used to store the files readied for commit
      */
     public static final File GITLET_STAGE_FOR_ADD_DIR = join(GITLET_DIR, "stageForAdd");
 
@@ -48,12 +48,12 @@ public class Repository {
     public static final File GITLET_BLOBS_DIR = join(GITLET_DIR, "blobs");
 
     /**
-     * The sha1 value of master branch
+     * The sha1 value of master branch pointer
      */
     public String masterSha1;
 
     /**
-     * The sha1 value of HEAD branch
+     * The sha1 value of HEAD pointer
      */
     public String HEADSha1;
 
@@ -73,31 +73,26 @@ public class Repository {
             GITLET_BLOBS_DIR.mkdir();
         }
 
-        File sentinelFile = new File("sentinel.txt");
+        File sentinelFile = new File(".gitletTemp.txt");
         sentinelFile.createNewFile();
         add(sentinelFile.toString());
         commit("init commit", null);
+        sentinelFile.delete();
     }
 
     public void add(String filename) throws IOException {
         File file = new File(filename);
         String fileSha1 = sha1(file);
+        //todo: check if this file is identical to the current commit
+
         Path src = file.toPath();
         Path dest = GITLET_STAGE_FOR_ADD_DIR.toPath();
         Files.copy(src, dest, StandardCopyOption.REPLACE_EXISTING);
     }
 
     public void commit(String message, String parentSha1) throws IOException {
-        Commit commit =
-                new Commit("test", "testSha1", GITLET_STAGE_FOR_ADD_DIR);
 
-        Path src = GITLET_STAGE_FOR_ADD_DIR.toPath();
-        Path dest = GITLET_BLOBS_DIR.toPath();
-        Files.copy(src, dest, StandardCopyOption.REPLACE_EXISTING);
-        File[] files = GITLET_BLOBS_DIR.listFiles();
-        for (File file : files) {
-            file.delete();
-        }
+
     }
 
 
