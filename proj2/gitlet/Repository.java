9bbe1a9535dbafe5abb5d67b209b8403fba7cde2
LongPeng <longpeng96@qq.com>
2023-01-09 18:45:39 +0800

    ConcatAndWriteContents() v2

--- proj2/gitlet/Repository.java
+++ proj2/gitlet/Repository.java
@@ -733,7 +733,7 @@ public class Repository {
     private static void ConcatAndWriteContents(String contentOfCurrFile,
                                                String contentOfTargetFile, String CWDFilename) {
         String resultContent = "<<<<<<< HEAD\n" + contentOfCurrFile
-                + "=======\n" + contentOfTargetFile + ">>>>>>>";
+                + "=======\n" + contentOfTargetFile + ">>>>>>>\n";
         File resultFile = join(CWD, CWDFilename);
         writeContents(resultFile, resultContent);
     }
